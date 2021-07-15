90408a8854fa51a5bea9a6708a80e9128f57b5c1
Jun-Kataoka <j.kataoka.biz@gmail.com>
2021-07-16 04:00:05 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,            presence: true
+  validates :family_name,         presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+  validates :first_name,          presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+  validates :family_name_reading, presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :first_name_reading,  presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :birthday,            presence: true, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}, length: { is: 8 }
+
+  has_many :items
+  has_many :orders
 end
