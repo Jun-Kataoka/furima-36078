50607b5ee84abb2a06c888d5ef11010596e60cf9
Jun-Kataoka <j.kataoka.biz@gmail.com>
2021-07-19 19:08:49 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :period
+  belongs_to :user
 end
