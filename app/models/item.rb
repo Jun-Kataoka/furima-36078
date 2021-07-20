4d58d15d988442bbb29fa24df476731368e73802
Jun-Kataoka <j.kataoka.biz@gmail.com>
2021-07-20 19:13:04 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,14 +12,16 @@ class Item < ApplicationRecord
     validates :image
     validates :name, length: { maximum: 40 }
     validates :caption, length: { maximum: 1000 }
-    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :fee_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-    validates :period_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :price, numericality: { greater_than_or_equal_to: 300,
                                       less_than_or_equal_to: 9_999_999,
                                       allow_blank: true,
                                       message: 'is invalid. Input  range of 300 to 9999999 numeric characters.' }
+  end
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :fee_id
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :period_id
   end
 end
