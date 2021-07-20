f29f4bbd4f67331cece1caf2268eba1a56af87b2
Jun-Kataoka <j.kataoka.biz@gmail.com>
2021-07-21 00:58:16 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :encrypted_password, :password, :password_confirmation,
-            format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, allow_blank: true, message: 'is invalid. Input must contain both alphameric and numeric characters.' }
-  validates :nickname, :birthday,                      presence: true
-  validates :family_name, :first_name,                 presence: true,
-                                                       format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true, message: 'is invalid. Input full-width characters.' }
-  validates :family_name_reading, :first_name_reading, presence: true,
-                                                       format: { with: /\A[ァ-ヶー]+\z/, allow_blank: true, message: 'is invalid. Input full-width katakana characters.' }
+            format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, allow_blank: true,
+                      message: 'is invalid. Input must contain both alphameric and numeric characters.' }
+  with_options presence: true do
+    validates :nickname, :birthday
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true,
+                           message: 'is invalid. Input full-width characters.' } do
+      validates :family_name, :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー]+\z/, allow_blank: true,
+                           message: 'is invalid. Input full-width katakana characters.' } do
+      validates :family_name_reading, :first_name_reading
+    end
+  end
 
   has_many :items
   has_many :orders
