36fe92a6bbe895e31822829215d3da29e11f0c2c
Jun-Kataoka <j.kataoka.biz@gmail.com>
2021-07-21 15:11:58 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
+  before_action :set_item, only: [:show, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,16 +20,20 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    if user_signed_in? && current_user.id == @item.user_id
+      @item.destroy
+    end
     redirect_to root_path
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item)
